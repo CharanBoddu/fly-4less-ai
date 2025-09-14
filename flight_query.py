af61d9334f5e94b5e5350bdcdaf17ce44b797ba1
Charan Boddu <cboddu@mun.ca>
2025-09-14 20:58:37 +0200

    Trying to scrape google flights for valid data

--- flight_query.py
+++ flight_query.py
@@ -2,6 +2,7 @@
 import os
 from google import genai
 from dotenv import load_dotenv
+from fast_flights import FlightData, Passengers, get_flights
 
 load_dotenv()
 
@@ -23,6 +24,7 @@ def extract_flight_details(user_input):
 
         Query: "{user_input}"
 
+        Return only the output JSON. Do not include explanations or any extra text.
         Return output in JSON format with:
         - departure airport code (airport code)
         - destination airport code (airport code)
@@ -31,6 +33,25 @@ def extract_flight_details(user_input):
         """])
 
     return response.text 
+
+
+def search_flights_fastflights(departure, destination, depart_date, return_date=None, adults=1, children=0):
+    flight_data = [FlightData(date=depart_date, from_airport=departure, to_airport=destination)]
+    trip_type = "one-way"
+    if return_date:
+        # We add return flight data if return_date is provided.
+        flight_data.append(FlightData(date=return_date, from_airport=destination, to_airport=departure))
+        trip_type = "round-trip"
+    passengers = Passengers(adults=adults, children=children, infants_in_seat=0, infants_in_lap=0)
+    results = get_flights(
+        flight_data=flight_data,
+        trip=trip_type,
+        seat="economy",
+        passengers=passengers,
+        fetch_mode="fallback"
+        )
+    return results
+    
 
 user_input = "Find me the cheapest flight from Toronto to New York from March 10-15, 2025"
 structured_data = extract_flight_details(user_input)
